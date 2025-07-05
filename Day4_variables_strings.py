72dacfe01fb59db9346aa2215cdf498f49bee8de
Uditi1801 <uditi18012008@gmail.com>
2025-07-06 00:03:24 +0530

    Add Day 4 practice: Python variables and strings

---
+++ Day4_variables_strings.py
@@ -0,0 +1,10 @@
+# Day 4 - Variables, Data Types, and Strings
+
+name = "Uditi"
+age = 18
+height = 5.3
+is_student = True
+
+print("Name:", name)
+print("Age + 2 =", age + 2)
+print("Uppercase:", name.upper())
